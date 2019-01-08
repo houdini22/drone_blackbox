243e71739bd3f7e2d337416e460f06ccbeb3001f
MM <baniczek@gmail.com>
2019-01-08 13:21:17 +0100

    new steering

--- MOJE/_ARDUINO/4_ppm_xbox_read_no_lcd/4_ppm_xbox_read_no_lcd.ino
+++ MOJE/_ARDUINO/4_ppm_xbox_read_no_lcd/4_ppm_xbox_read_no_lcd.ino
@@ -1,27 +1,24 @@
 ////////////////////// PPM CONFIGURATION ///////////////////////////////
-#define CHANNELS_NUMBER       4           // set the number of chanels
+#define CHANNELS_NUMBER       8           // set the number of chanels
 #define DEFAULT_SERVO_VALUE   1500        // set the default servo value
-#define PPM_FRAME_LENGTH      12500       // set the PPM frame length in microseconds (1ms = 1000µs) (2.5ms * (NUM_CHANNELS + 1)
+#define PPM_FRAME_LENGTH      22500       // set the PPM frame length in microseconds (1ms = 1000µs) (2.5ms * (NUM_CHANNELS + 1)
 #define PPM_PULSE_LENGTH      300         // set the pulse length
 #define POLARITY              1           // set polarity of the pulses: 1 is positive, 0 is negative
 #define SIGNAL_PIN            2           // set PPM signal output pin on the arduino
+#define LED_PIN               3           // set PIN of LED
 ///////////////////////////////////////////////////////////////////////
 
 #include <Wire.h>
 
 ////////////////////// GLOBAL VARIABLES ///////////////////////////////
-const unsigned int MAX_INPUT = 50;
-int defaultPpmValues[CHANNELS_NUMBER] = {1500, 1500, 1100, 1500};
-int ppm[CHANNELS_NUMBER] = {1500, 1500, 1100, 1500};
+const unsigned int MAX_INPUT = 20;
+int defaultPpmValues[CHANNELS_NUMBER] = {1500, 1500, 1100, 1500, 1100, 1100, 1100, 1100};
+int ppm[CHANNELS_NUMBER] = {1500, 1500, 1100, 1500, 1100, 1100, 1100, 1100};
 unsigned long lastReceivedPing = 0;
+bool connected = false;
 ///////////////////////////////////////////////////////////////////////
 
-void setup() {
-  Serial.begin(115200);
-
-  pinMode(SIGNAL_PIN, OUTPUT);
-  digitalWrite(SIGNAL_PIN, !POLARITY);    // set the PPM signal pin to the default state (off)
-
+void startTimer() {
   cli();
   TCCR1A = 0;                             // set entire TCCR1 register to 0
   TCCR1B = 0;
@@ -32,46 +29,67 @@ void setup() {
   TIMSK1 |= (1 << OCIE1A);                // enable timer compare interrupt
   sei();
 
-  Serial.write("r");
+  digitalWrite(LED_PIN, HIGH);
 }
 
-void processData(const char *data) {
-  String str(data);
+void stopTimer() {
+  TCCR1B = 0;
+  TIMSK1 |= (0 << OCIE1A);
 
-  int index1 = str.indexOf(' ');
-  int index2 = str.indexOf(' ', index1 + 1);
-  int index3 = str.indexOf(' ', index2 + 1);
+  digitalWrite(LED_PIN, LOW);
+}
 
-  int ch1 = str.substring(0, index1).toInt();
-  int ch2 = str.substring(index1 + 1, index2).toInt();
-  int ch3 = str.substring(index2 + 1, index3).toInt();
-  int ch4 = str.substring(index3 + 1, str.length()).toInt();
+void setup() {
+  Serial.begin(115200);
 
-  ppm[0] = ch3;
-  ppm[1] = ch4;
-  ppm[2] = ch2;
-  ppm[3] = ch1;
+  pinMode(SIGNAL_PIN, OUTPUT);
+  digitalWrite(SIGNAL_PIN, !POLARITY);    // set the PPM signal pin to the default state (off)
 }
 
 void processIncomingByte(const byte inByte) {
-  static char input_line[MAX_INPUT];
   static unsigned int input_pos = 0;
   static bool started = false;
+  static char numChannel[1] = {0};
+  static char channelValue[4];
+  static bool channelValueStarted = false;
+  int numChannelInt = 0;
 
   switch (inByte) {
+    case 'z':
+      break;
+
     case '^':                                   // begin data packet
       input_pos = 0;
-      memset(input_line, 0, MAX_INPUT);
+      memset(channelValue, 0, 4);
       started = true;
       break;
 
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      if (started) {
+        numChannel[0] = inByte;
+        started = false;
+      } else if (channelValueStarted) {
+        channelValue[input_pos++] = inByte;
+      }
+
+    case '#':
+      channelValueStarted = true;
+      break;
+
     case '$':                                   // end data packet
-      input_line[input_pos] = 0;
       input_pos = 0;
-      if (started) {                            // make sure if full packet
-          processData(input_line);
-          started = false;
-      }            
+      numChannelInt = atoi(numChannel);
+      ppm[numChannelInt] = atoi(channelValue);
+      channelValueStarted = false;
       break;
 
     case 'p':                                   // ping
@@ -79,9 +97,18 @@ void processIncomingByte(const byte inByte) {
       Serial.write("p");
       break;
 
-    default:                                    // append to data packet
-      if (started && input_pos < (MAX_INPUT - 1))
-          input_line[input_pos++] = inByte;
+    case 'h':
+      lastReceivedPing = millis();
+      Serial.write("h");
+      connected = true;
+      break;
+
+    case 'n':
+      startTimer();
+      break;
+
+    case 'f':
+      stopTimer();
       break;
   }
 }
@@ -90,11 +117,9 @@ void loop() {
   while (Serial.available() > 0)
     processIncomingByte(Serial.read());
 
-  if (millis() - lastReceivedPing > 100) {
-    ppm[0] = defaultPpmValues[0];
-    ppm[1] = defaultPpmValues[1];
-    ppm[2] = defaultPpmValues[2];
-    ppm[3] = defaultPpmValues[3];
+  if (millis() - lastReceivedPing > 500) {
+    stopTimer();
+    connected = false;
   }
 }
 
